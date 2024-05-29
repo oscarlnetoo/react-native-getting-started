82006d5207ec15498ee3571424a3605730d02e04
Oscar Neto <oscar.neto@icarotech.com>
2024-05-29 11:25:43 -0300

    feat: SafeAreaView component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 
 export const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello world!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello world!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
   container: {
     flex: 1,
     backgroundColor: '#121214',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
   },
   title: {
     color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
