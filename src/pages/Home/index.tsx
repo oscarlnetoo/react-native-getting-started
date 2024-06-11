478663fcb7311a7573444c138a7d0d6359ca9d6e
Oscar Neto <oscar.neto@icarotech.com>
2024-06-11 10:00:44 -0300

    feat: create a new component to handle task list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,9 +7,9 @@ import {
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
-  ScrollView,
   FlatList,
 } from 'react-native';
+import {TaskList} from '../../components/TaskList';
 
 interface Task {
   id: string;
@@ -48,15 +48,7 @@ export const Home = () => {
 
         <Text style={styles.titleTasks}>My Tasks</Text>
 
-        <FlatList
-          data={tasks}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => (
-            <TouchableOpacity style={styles.buttonTask}>
-              <Text style={styles.titleTask}>{item.title}</Text>
-            </TouchableOpacity>
-          )}
-        />
+        <TaskList tasks={tasks} />
       </View>
     </SafeAreaView>
   );
@@ -102,18 +94,6 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#121214',
     fontSize: 18,
-    fontWeight: 'bold',
-  },
-  buttonTask: {
-    backgroundColor: '#29292e',
-    padding: 10,
-    marginTop: 10,
-    borderRadius: 7,
-    alignItems: 'center',
-  },
-  titleTask: {
-    color: '#f1f1f1',
-    fontSize: 20,
     fontWeight: 'bold',
   },
 });
