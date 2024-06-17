276bb64cece5c5a21dadc5fc0c9851b3bdc2e5d7
Oscar Neto <oscar.neto@icarotech.com>
2024-06-17 10:57:19 -0300

    feat: create addTask method inside context

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,17 +1,45 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 
-export interface ITasksContext {
+export interface ITask {
   id: string;
   title: string;
 }
+
+export interface ITasksContext {
+  tasks: ITask[];
+  addTask(task: ITask): void;
+}
+
+const tasksData = '@MyTasks:Tasks';
 
 export const TasksContext = React.createContext<ITasksContext>(
   {} as ITasksContext,
 );
 
 export const TasksProvider: React.FunctionComponent = ({children}) => {
+  const [data, setData] = React.useState<ITask[]>([]);
+
+  React.useEffect(() => {
+    async function loadTasks() {
+      const taskList = await AsyncStorage.getItem(tasksData);
+
+      if (taskList) {
+        setData(JSON.parse(taskList));
+      }
+    }
+
+    loadTasks();
+  }, []);
+
+  const addTask = async (task: ITask) => {
+    const newTaskList = [...data, task];
+    setData(newTaskList);
+    await AsyncStorage.setItem(tasksData, JSON.stringify(newTaskList));
+  };
+
   return (
-    <TasksContext.Provider value={{id: '1', title: 'Task 1'}}>
+    <TasksContext.Provider value={{tasks: data, addTask}}>
       {children}
     </TasksContext.Provider>
   );
