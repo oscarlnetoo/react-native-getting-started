e6fa84fad56e3b8c0ce499466d5f0e587b5608a5
Oscar Neto <oscar.neto@icarotech.com>
2024-07-02 11:51:53 -0300

    feat: working with google fonts

--- devprofile/App.tsx
+++ devprofile/App.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Home } from './src/pages/Home';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
