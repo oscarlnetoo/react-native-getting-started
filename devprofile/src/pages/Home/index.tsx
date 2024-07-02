5b675c85ed512f959be2fe9efe68de7025ea45bc
Oscar Neto <oscar.neto@icarotech.com>
2024-07-02 14:54:10 -0300

    feat: create header from home page

--- devprofile/src/pages/Home/index.tsx
+++ devprofile/src/pages/Home/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Container, Header } from './styles';
+import {
+  Container,
+  Header,
+  Icon,
+  UserAvatar,
+  UserAvatarButton,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+} from './styles';
+
+import avatarDefault from '../../assets/avatar02.png';
 
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Header />
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Oscar</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
--- devprofile/src/pages/Home/styles.ts
+++ devprofile/src/pages/Home/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
-import { RFPercentage } from 'react-native-responsive-fontsize';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
   flex: 1;
@@ -9,6 +10,52 @@ export const Container = styled.View`
 export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(17)}px;
-  font-size: 24px;
+
   background-color: ${({ theme }) => theme.colors.secondary};
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding-top: ${RFValue(28)}px;
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: ${RFValue(52)}px;
+  height: ${RFValue(52)}px;
+  border-radius: 10px;
+`;
+
+export const UserInfoDetail = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.gray800};
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(28)}px;
+  color: ${({ theme }) => theme.colors.dark};
 `;
