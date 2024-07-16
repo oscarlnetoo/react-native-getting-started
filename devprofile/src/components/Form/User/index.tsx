c40199646a580cd1ba3d4d1470a94e8ef874657b
Oscar Neto <oscar.neto@icarotech.com>
2024-07-16 11:52:34 -0300

    feat: create user component

---
+++ devprofile/src/components/Form/User/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import avatarDefault from '../../assets/avatar02.png';
+import {
+  Container,
+  EmailData,
+  EmailTitle,
+  NameData,
+  NameTitle,
+  UserAvatar,
+  UserDetail,
+  UserEmailDetail,
+  UserNameDetail,
+} from './styles';
+import { IUser } from '../../../model/user';
+
+interface UserProps {
+  data: IUser;
+  onPress: () => void;
+}
+
+export const User: React.FunctionComponent<UserProps> = ({ data, onPress }) => {
+  return (
+    <Container onPress={onPress}>
+      <UserDetail>
+        <UserNameDetail>
+          <NameTitle>NAME</NameTitle>
+          <NameData>{data.name}</NameData>
+        </UserNameDetail>
+        <UserEmailDetail>
+          <EmailTitle>EMAIL</EmailTitle>
+          <EmailData>{data.email}</EmailData>
+        </UserEmailDetail>
+      </UserDetail>
+      <UserAvatar
+        source={data.avatar_url ? { uri: data.avatar_url } : avatarDefault}
+      />
+    </Container>
+  );
+};
